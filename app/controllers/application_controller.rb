7d4d49e7187336c3e449caf18f40ea3423b773ec
Adam Elhardt <adam.elhardt@mangolanguages.com>
2024-10-09 21:46:59 -0400

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  http_basic_authenticate_with name: ENV['BASIC_USERNAME'], password: ENV['BASIC_PWD']
 end
