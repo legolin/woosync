c07b810ee4f9bdda6972c9f509730ef276803617
Adam Elhardt <adam.elhardt@mangolanguages.com>
2024-10-09 22:17:01 -0400

    Don't use relative import with importmaps

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,7 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import consumer from '../channels/consumer'
+import consumer from 'channels/consumer'
 import CableReady from 'cable_ready'
 
 CableReady.initialize({ consumer })
